af573d000608d20df90cf80d2ce922a9cb5f7960
Nicholas O Rawlings <nicholas@itecto.net>
2019-08-20 22:58:22 -0400

    Initial commit with grid drawing script

---
+++ grid.py
@@ -0,0 +1,43 @@
+import argparse
+import math
+import sys
+import turtle
+
+parser = argparse.ArgumentParser(description='Draw a grid')
+parser.add_argument('-r', '--rows', type=int, required=True, help='number of rows')
+parser.add_argument('-c', '--columns', type=int, required=True, help='number of columns')
+argv = parser.parse_args()
+
+rows = argv.rows
+if rows < 1:
+    print('Grid must have at least one row', file=sys.stderr)
+    sys.exit(1)
+
+cols = argv.columns
+if cols < 1:
+    print('Grid must have at least one column', file=sys.stderr)
+    sys.exit(1)
+
+def drawLine(x1, y1, x2, y2):
+    t.penup()
+    t.hideturtle()
+    t.goto(x1, y1)
+    t.pendown()
+    t.showturtle()
+    t.goto(x2, y2)
+
+t = turtle.Turtle()
+screen = turtle.Screen()
+width, height = screen.screensize()
+
+for i in range(cols + 1):
+    x = width / cols * i
+    print(f'Column boundary {i} at {x}')
+    drawLine(x, 0, x, height)
+
+for i in range(rows + 1):
+    y = height / rows * i
+    print(f'Row boundary {i} at {y}')
+    drawLine(0, y, width, y)
+
+turtle.done()
