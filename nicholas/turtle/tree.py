e232ac1fa2f0c3e1c52b0880ecbd42c056cab2b9
Nicholas O Rawlings <nicholas@itecto.net>
2019-08-29 10:44:34 -0400

    Draw tree breadth-first

--- nicholas/turtle/tree.py
+++ nicholas/turtle/tree.py
@@ -1,26 +1,45 @@
 import argparse
 import turtle
 
-t = turtle.Turtle()
-t.hideturtle()
-t.setheading(90)
+class Tree(turtle.Turtle):
+	
+	left = None
+	right = None
+	drawn = False
+
+	def __init__(self, position, heading, length=200):
+		turtle.Turtle.__init__(self)
+		self.hideturtle()
+		self.penup()
+		self.setposition(position)
+		self.setheading(heading)
+		self.length = length
+		self.pendown()
+	
+	def draw(self, minBranchLength=5):
+		if self.length < minBranchLength:
+			self.dot(minBranchLength, '#009900')
+			return True
+		elif self.left == None or self.right == None:
+			self.forward(self.length)
+			self.left = Tree(self.position(), self.heading() - 30, self.length * 0.5)
+			self.right = Tree(self.position(), self.heading() + 30, self.length * 0.5)
+			self.drawn = True
+			return False
+		else:
+			leftDone = self.left.draw(minBranchLength)
+			rightDone = self.right.draw(minBranchLength)
+			return leftDone and rightDone
+
 
 screen = turtle.Screen()
 screen.tracer(1, 0)
 
-def branch(t, length):
-    t.forward(length)
+tree = Tree((0, -200), 90, 200)
+tree.numBranches = 3
+done = False
 
-    length = length * 0.55
-    if length > 1:
+while not done:
+	done = tree.draw(5)
 
-        left = t.clone()
-        left.left(30)
-        branch(left, length)
-
-        right = t.clone()
-        right.right(30)
-        branch(right, length)
-
-branch(t, 200)
 turtle.done()
