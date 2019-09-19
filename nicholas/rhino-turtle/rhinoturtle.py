29d143b8bfc8b3be976f027f2b489a7ad62d51fa
Nicholas O Rawlings <nicholas@itecto.net>
2019-09-19 02:26:39 -0400

    Record position and orientation as a plane to better handle navigation in 3D space

--- nicholas/rhino-turtle/rhinoturtle.py
+++ nicholas/rhino-turtle/rhinoturtle.py
@@ -1,50 +1,75 @@
 import rhinoscriptsyntax as rs
 from Rhino.RhinoMath import ToRadians
-from Rhino.Geometry import Point3d, Vector3d
+from Rhino.Geometry import Plane, Point3d, Vector3d
 from System.Drawing import Color
 
 class Turtle:
     
-    _position = Point3d(0, 0, 0)
-    _heading = Vector3d(1, 0, 0)  # Must be a unit vector
-    _penDown = True
-    _color = Color.FromArgb(0)
+    def __init__(self):
+        self._pose = Plane.WorldXY
+        self._penDown = True
+        self._color = Color.FromArgb(0)
+    
+    def position(self):
+        return self._pose.Origin
+    
+    def heading(self):
+        return self._pose.XAxis
     
     def forward(self, distance):
-        self._moveTo(self._position + (self._heading * distance))
+        self.move(self.heading() * distance)
     
     def back(self, distance):
         self.forward(-distance)
     
     def left(self, angle):
-        self._heading.Rotate(ToRadians(angle), Vector3d(0, 0, 1))
+        self._pose.Rotate(ToRadians(angle), self._pose.ZAxis)
     
     def right(self, angle):
         self.left(-angle)
         
+    def down(self, angle):
+        self._pose.Rotate(ToRadians(angle), self._pose.YAxis)
+    
+    def up(self, angle):
+        self.down(-angle)
+    
+    def rollLeft(self, angle):
+        self._pose.Rotate(ToRadians(angle), self._pose.XAxis)
+    
+    def rollRight(self, angle):
+        self.left(-angle)
+        
     def setPosition(self, x, y=0, z=0):
         if isinstance(x, Point3d):
-            self._moveTo(x)
+            self.moveTo(x)
         else:
-            self._moveTo(Point3d(x, y, z))
+            self.moveTo(Point3d(x, y, z))
             
     def setX(self, x):
-        self._moveTo(Point3d(x, self._position.y, self._position.z))
+        self.moveTo(Point3d(x, self._pose.OriginY, self._pose.OriginZ))
             
     def setY(self, y):
-        self._moveTo(Point3d(self._position.x, y, self._position.z))
+        self.moveTo(Point3d(self._pose.OriginX, y, self._pose.OriginZ))
             
     def setZ(self, z):
-        self._moveTo(Point3d(self._position.x, self._position.y, z))
-    
-    def _moveTo(self, newPosition):
+        self.moveTo(Point3d(self._pose.OriginX, self._pose.OriginY, z))
+        
+    def move(self, vector):
         if self._penDown:
-            line = rs.AddLine(self._position, newPosition)
+            newPosition = self._pose.Origin + vector
+            line = rs.AddLine(self._pose.Origin, newPosition)
             rs.ObjectColor(line, self._color)
-        self._position = newPosition
+        self._pose.Translate(vector)
+    
+    def moveTo(self, newPosition):
+        if self._penDown:
+            line = rs.AddLine(self._pose.Origin, newPosition)
+            rs.ObjectColor(line, self._color)
+        self._pose.Origin = newPosition
         
     def setHeading(self, newHeading):
-        self._heading = newHeading
+        self._pose.XAxis = newHeading
     
     def setColor(self, color):
         self._color = color
@@ -57,13 +82,20 @@ class Turtle:
 
 
 t = Turtle()
-t.penUp()
-t.setPosition(-15, 10, 0)
-t.penDown()
 t.forward(10)
 t.left(45)
 t.setColor(Color.FromArgb(255, 0, 0))
 t.forward(10)
-t.right(90)
+t.right(45)
 t.setColor(Color.FromArgb(0, 0, 255))
 t.forward(10)
+
+
+t2 = Turtle()
+t2.forward(10)
+t2.up(45)
+t2.setColor(Color.FromArgb(255, 0, 0))
+t2.forward(10)
+t2.down(45)
+t2.setColor(Color.FromArgb(0, 0, 255))
+t2.forward(10)
