91e7f56024c24ac9ebe23bffb942d7d8354d2c9f
Nicholas O Rawlings <nicholas@itecto.net>
2019-09-23 02:32:43 -0400

    Organization

--- nicholas/rhino-turtle/rhinoturtle.py
+++ nicholas/rhino-turtle/rhinoturtle.py
@@ -11,8 +11,12 @@ class Turtle(object):
         self.color = Color.FromArgb(0)
         self.width = 0
 
+
+    # Position properties and methods
+
     @property
     def position(self):
+        """The turtle's current position."""
         return self._pose.Origin
 
     @position.setter
@@ -20,40 +24,6 @@ class Turtle(object):
         if self._penDown:
             self._drawLine(newPosition)
         self._pose.Origin = newPosition
-
-    @property
-    def heading(self):
-        return self._pose.XAxis
-
-    @heading.setter
-    def heading(self, newHeading):
-        vector = Vector3d(newHeading)
-        vector.Unitize()
-        self._pose.XAxis = vector
-
-    def forward(self, distance):
-        self.move(self.heading * distance)
-
-    def back(self, distance):
-        self.forward(-distance)
-
-    def left(self, angle):
-        self._pose.Rotate(ToRadians(angle), self._pose.ZAxis)
-
-    def right(self, angle):
-        self.left(-angle)
-
-    def down(self, angle):
-        self._pose.Rotate(ToRadians(angle), self._pose.YAxis)
-
-    def up(self, angle):
-        self.down(-angle)
-
-    def rollLeft(self, angle):
-        self._pose.Rotate(ToRadians(angle), self._pose.XAxis)
-
-    def rollRight(self, angle):
-        self.left(-angle)
 
     @property
     def x(self):
@@ -85,63 +55,75 @@ class Turtle(object):
     def goto(self, x=None, y=None, z=None):
         self.position = Point3d(x or self.X, y or self.Y, z or self.Z)
 
+
+    # Heading properties and methods
+
+    @property
+    def heading(self):
+        """The turtle's current heading."""
+        return self._pose.XAxis
+
+    @heading.setter
+    def heading(self, newHeading):
+        vector = Vector3d(newHeading)
+        vector.Unitize()
+        self._pose.XAxis = vector
+
+    def left(self, angle):
+        """Turn the turtle `angle` degrees to the left."""
+        self._pose.Rotate(ToRadians(angle), self._pose.ZAxis)
+
+    def right(self, angle):
+        """Turn the turtle `angle` degrees to the right."""
+        self.left(-angle)
+
+    def down(self, angle):
+        """Turn the turtle downward by `angle` degrees."""
+        self._pose.Rotate(ToRadians(angle), self._pose.YAxis)
+
+    def up(self, angle):
+        """Turn the turtle upward `angle` degrees."""
+        self.down(-angle)
+
+    def rollLeft(self, angle):
+        """Roll the turtle `angle` degrees to the left."""
+        self._pose.Rotate(ToRadians(angle), self._pose.XAxis)
+
+    def rollRight(self, angle):
+        """Roll the turtle `angle` degrees to the right."""
+        self.left(-angle)
+
+
+    # Movement methods
+
     def move(self, vector):
+        """Move the turtle according to the given vector."""
         if self._penDown:
             self._drawLine(self._pose.Origin + vector)
         self._pose.Translate(vector)
 
-    def _drawLine(self, newPosition):
-        line = rs.AddLine(self._pose.Origin, newPosition)
+    def _drawLine(self, endPoint):
+        """Draw a line from the turtle's current position to `endPoint`."""
+        line = rs.AddLine(self._pose.Origin, endPoint)
         rs.ObjectColor(line, self.color)
         rs.ObjectPrintColor(line, self.color)
         rs.ObjectPrintWidth(line, self.width)
 
-    def penUp(self):
-        self._penDown = False
+    def forward(self, distance):
+        """Move the turtle forward by the specified `distance`."""
+        self.move(self.heading * distance)
 
-    def penDown(self):
+    def back(self, distance):
+        """Move the turtle backward by the specified `distance`."""
+        self.forward(-distance)
+
+
+    # Pen control methods
+
+    def pendown(self):
+        """Put the pen down so the turtle draws when moving."""
         self._penDown = True
 
-
-# Clear the board
-rs.DeleteObjects(rs.AllObjects(select=True))
-
-red = Color.FromArgb(255, 0, 0)
-green = Color.FromArgb(0, 255, 0)
-blue = Color.FromArgb(0, 0, 255)
-
-t = Turtle()
-t.penUp()
-t.X = -5
-t.Y = -5
-t.Z = -5
-t.penDown()
-
-t.color = red
-t.forward(10)
-t.up(90)
-
-t.width = 1
-t.color = blue
-t.forward(10)
-t.left(90)
-
-t.width = 2
-t.color = green
-t.forward(10)
-t.up(90)
-
-t.width = 3
-t.color = red
-t.forward(10)
-t.left(90)
-
-t.width = 4
-t.color = blue
-t.forward(10)
-t.up(90)
-
-t.width = 5
-t.color = green
-t.forward(10)
-t.left(90)
+    def penup(self):
+        """Pull the pen up so the turtle does not draw when moving."""
+        self._penDown = False
