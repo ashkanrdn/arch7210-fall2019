169d78b764147dacbb80574db60086ce82e6b8dc
Nicholas <norawlin@uncc.edu>
2019-12-08 00:39:50 -0500

    Eyes Matrix

--- robot/Readme.md
+++ robot/Readme.md
@@ -13,11 +13,15 @@ $ sudo pip3 install lomond
 
 Copy the unit file to the systemd folder:
 ```
-$ sudo cp arch7210-fall2019/robot/robot.service /etc/systemd/system
+$ sudo cp arch7210-fall2019/robot/*.service /etc/systemd/system
 ```
 
 Enable the service:
 ```
 $ sudo systemctl start robot.service
 $ sudo systemctl enable robot.service
+
+$ sudo systemctl start pigpio.service
+$ sudo systemctl enable pigpio.service
+
 ```
--- robot/eyematrix.py
+++ robot/eyematrix.py
@@ -7,9 +7,33 @@ import board
 from adafruit_ht16k33.matrix import Matrix8x8x2
 i2c = busio.I2C(board.SCL, board.SDA)
 
-gridA = Matrix8x8x2(i2c,address=0x70) 
-gridB = Matrix8x8x2(i2c,address=0x74)
+gridA = Matrix8x8x2(i2c,address=0x70,auto_write = False) 
+gridB = Matrix8x8x2(i2c,address=0x74,auto_write = False)
 
-happy = matrix.loadImage("/home/pi/arch7210-fall2019/robot/happy.png")
-happy.apply(gridB)
-happy.rotateRight().apply(gridA)
+normal_center = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/normal_center.png")
+normal_left = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/normal_left.png")
+normal_right = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/normal_right.png")
+
+while True:
+
+    normal_center.apply(gridB)
+    normal_center.apply(gridA)
+    time.sleep(0.5)
+    
+    normal_left.apply(gridA)
+    normal_left.apply(gridB)
+    time.sleep(0.5)
+    
+    normal_center.apply(gridB)
+    normal_center.apply(gridA)
+    time.sleep(0.5)
+    
+    
+    normal_right.apply(gridA)
+    normal_right.apply(gridB)
+    time.sleep(0.5)
+#happy.rotateRight().apply(gridA)
+
+
+
+
