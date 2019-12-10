09fbaed0b48e8bc3370570d2646bbcc5e4e478d4
Pablo <pablo@uncc.edu>
2019-12-10 04:12:29 -0500

    updated eyes animations

--- robot/eyematrix.py
+++ robot/eyematrix.py
@@ -1,15 +1,25 @@
 import matrix
 import busio
 import board
-
+import time
 from adafruit_ht16k33.matrix import Matrix8x8x2
 i2c = busio.I2C(board.SCL, board.SDA)
+
+"""Links to all the images used for the eyes"""
 
 HAPPY_CENTER = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/happy_center.png")
 HAPPY_CORNER = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/happy_left.png")
 NORMAL_CENTER = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/normal_center.png")
 NORMAL_LEFT = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/normal_left.png")
+NORMAL_CORNER = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/normal_corner.png")
+NORMAL_TOP = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/normal_up.png")
 ANGRY_CENTER = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/angry_center.png")
+ANGRY_LEFT = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/angry_left.png")
+ANGRY_RIGHT = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/angry_right.png")
+BLINK_HALFOPEN = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/blink_halfway_open.png")
+BLINK_HALFCLOSED = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/blink_halfway_closed.png")
+BLINK_CLOSED = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/blink_closed.png")
+TIRED = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/tired_center.png")
 
 class RobotEyes:
     
@@ -34,6 +44,8 @@ class RobotEyes:
         """Apply mirrored image to both eyes"""
         self.applyRight(image)
         self.applyLeft(image.flip())
+
+    """=============HAPPY EYES============="""
         
     def happy_center(self):
         """Generate happy eyes in center of matrix"""
@@ -46,6 +58,8 @@ class RobotEyes:
     def happy_right(self):
         """Generate happy eyes in right of matrix"""
         self.applyBoth(HAPPY_CORNER)
+        
+    """=============NORMAL EYES============="""
 
     def normal_center(self):
         """Generate normal eyes in center of matrix"""
@@ -58,16 +72,168 @@ class RobotEyes:
     def normal_right(self):
         """Generate normal eyes in right of matrix"""
         self.applyBoth(NORMAL_LEFT)
+        
+    def normal_up(self):
+        """Generate normal eyes in top center of matrix"""
+        self.applyBoth(NORMAL_TOP)
+        
+    def normal_down(self):
+        """Generate normal eyes in bottom center of matrix"""
+        self.applyBoth(NORMAL_TOP.flop())
+
+    def normal_down_left(self):
+        """Generate normal eyes in left bottom corner of matrix"""
+        self.applyBoth(NORMAL_CORNER)
+        
+    def normal_down_right(self):
+        """Generate normal eyes in right bottom corner of matrix"""
+        self.applyBoth(NORMAL_CORNER.flip())
+        
+    def normal_up_left(self):
+        """Generate normal eyes in left upper corner of matrix"""
+        self.applyBoth(NORMAL_CORNER.flop())
+
+    def normal_up_right(self):
+        """Generate normal eyes in right upper corner of matrix"""
+        self.applyBoth(NORMAL_CORNER.transpose())
+        
+    """=============ANGRY EYES============="""
     
     def angry_center(self):
-        self.applyLeft(ANGRY_CENTER.flip())
-        self.applyRight(ANGRY_CENTER)
+        """Generate angry eyes in center of matrix"""
+        self.applyMirrored(ANGRY_CENTER)
         
+    def angry_right(self):
+        """Generate angry eyes in right of matrix"""
+        self.applyLeft(ANGRY_RIGHT)
+        self.applyRight(ANGRY_LEFT)
+        
+    def angry_left(self):
+        """Generate angry eyes in left of matrix"""
+        self.applyLeft(ANGRY_LEFT.flip())
+        self.applyRight(ANGRY_RIGHT.flip())
+        
+    """=============TIRED EYES============="""
+    
+    def tired_eyes(self):
+        """Generate tired eyes in the center of matrix"""
+        self.applyBoth(TIRED)
+        
+        
+    """=============SAD EYES============="""
+    #Still needs work
+    def sad_center(self):
+        """Generate tired eyes in the center of matrix"""
+        self.applyBoth(TIRED)
+        
+    def sad_left(self):
+        """Generate tired eyes in the center of matrix"""
+        self.applyBoth(TIRED)
+        
+    def sad_right(self):
+        """Generate tired eyes in the center of matrix"""
+        self.applyBoth(TIRED)
+        
+    """=============BLINK EYES============="""
+    
+    def halfway_open(self):
+        """Eyes closed 3/4 of the way"""
+        self.applyBoth(BLINK_HALFOPEN)
+        
+    def halfway_closed(self):
+        """Eyes closed 1/2 of the way"""
+        self.applyBoth(BLINK_HALFCLOSED)
+        
+    def closed(self):
+        """Eyes closed"""
+        self.applyBoth(BLINK_CLOSED)
+        
+    """=============ANIMATIONS============="""
+    
+    def animate(self, actions, duration):
+        """List of actions and time duration for each eyes transition"""
+        actions[0]()
+        sleepTime = duration / (len(actions) - 1)
+        
+        for action in actions[1:]:
+            time.sleep(sleepTime)
+            action()
+    
+    def blink(self, duration=0.25):
+        """Eyes blink"""
+        self.animate([
+            self.normal_center,
+            self.halfway_open,
+            self.halfway_closed,
+            self.closed,
+            self.halfway_closed,
+            self.halfway_open,
+            self.normal_center
+        ], duration)
+
+    def bored(self, duration = 1.2):
+        """Rolling eyes"""
+        self.animate([
+            self.normal_center,
+            self.normal_left,
+            self.normal_up_left,
+            self.normal_up,
+            self.normal_up_right,
+            self.normal_right,
+            self.normal_center
+        ], duration)
+        
+    def tired(self,duration = 1):
+        """Eyes clossing and changing to yellow"""
+        self.animate([
+            self.normal_center,
+            self.halfway_open,
+            self.halfway_closed,
+            self.closed,
+            self.tired_eyes
+        ], duration)
+
+
+    def wander_left_right(self, duration = 1):
+        """Eyes moving left and right"""
+        self.animate([
+            self.normal_center,
+            self.normal_left,
+            self.normal_center,
+            self.normal_right,
+            self.normal_center,
+        ], duration)
+        
+    def wander_around(self, duration = 1):
+        """Eyes moving in an diagonal pattern"""
+        self.animate([
+            self.normal_center,
+            self.normal_up_left,
+            self.normal_up,
+            self.normal_up_right,
+            self.normal_center,
+            self.normal_down_left,
+            self.normal_down,
+            self.normal_down_right,
+            self.normal_center
+        ], duration)
+        
+        
+    def angry_stare(self, duration = 1):
+        self.animate([
+            self.angry_center,
+            self.angry_left,
+            self.angry_center,
+            self.angry_right,
+            self.angry_center
+        ], duration)
+                
+#Still missing sad animation GITHUB not working
+
 eyes = RobotEyes()
-eyes.angry_center()
+eyes.angry_stare()
 
 """
-
 normal_left = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/normal_left.png")
 normal_right = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/normal_right.png")
 angry_right = matrix.loadImage("/home/pi/arch7210-fall2019/robot/eyes/angry_center.png")
--- robot/robot.py
+++ robot/robot.py
@@ -4,8 +4,7 @@ import RPi.GPIO as GPIO
 import os
 import sys
 import servomotor
-
-print(__file__)
+import eyematrix
 
 # Modify PATH so we can import files from elsewhere in this repo
 sys.path.append(os.path.dirname(__file__) + "/..")
@@ -14,8 +13,10 @@ from nicholas.robot.stepper import Motor, PhysicalAxle
 from nicholas.robot.turtlebot import Turtle
 from nicholas.robot.wsjsonrpc import JSONRPCWebSocket
 
+from lomond.persist import persist
 
-GPIO.setmode(GPIO.BOARD)
+
+#GPIO.setmode(GPIO.BOARD)
 
 # Set up the motors
 left = Motor(7, 11, 13, 15)
@@ -28,18 +29,32 @@ distance_between_wheels = 9.5625
 axle = PhysicalAxle(left, right, microsteps_per_revolution, circumference_of_wheel, distance_between_wheels, 0.01)
 
 pen = servomotor.ServoPen()
+eyes = eyematrix.RobotEyes()
 
 # Create the robot
 robot = Turtle(axle, pen)
 
 # Set up the WebSocket connection
 ws = JSONRPCWebSocket("wss://arch7210-fall2019.coditect.com/pablo")
-ws.set_basic_auth("pablo", "KarduniKlub")
+ws.set_basic_auth("pablo", "*************")
 
 try:
 	for request in ws.requests():
+		#stop blinking
 		request.exec(robot, ws)
-
+		#resume blinking... if no more commands in the next n seconds
+		
+		#if no more commands in the next n seconds / show bored
+	"""
+	for event in persist(ws):
+		print(event)
+		request = ws.get_request(event)
+		if request is not None:
+			request.exec(robot, ws)
+		else:
+			eyes.blink()
+		
+	"""
 except KeyboardInterrupt:
 	pass
 
